66be9b23f77173aa1b6ff11b0f7447c0a02ee96e
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2024-07-19 12:17:36 -0400

    add audio

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.getElementById('snake-game')
 const ctx = canvas.getContext('2d')
 const score = document.getElementById('score')
 const highScore = document.getElementById('high-score')
+canvas.width = 400
+canvas.height = 400
 
 highScore.textContent = window.localStorage.getItem('highScore') || score.textContent
-const FPS = 100
-const sizeSquareMap = canvas.width / 15
-const totalSquare = canvas.width * canvas.height / Math.pow(sizeSquareMap, 2)
+const FPS = 10
+const interval = 1000 / FPS
+const numberOfSquare = 8
+const sizeSquare = canvas.width / numberOfSquare
+const totalSquare = (canvas.width * canvas.height) / Math.pow(sizeSquare, 2)
+
+const turnAudio = new Audio('./audio/turn.mp3')
+turnAudio.playbackRate = 2.5
+const deadAudio = new Audio('./audio/dead.mp3')
+const eatAudio = new Audio('./audio/eat.mp3')
 
 const level = {
   score,
@@ -14,11 +23,9 @@ const level = {
   win: false,
   gameOver: false,
   setIntervalId: null,
-  gridLineSize: 0.2,
-  gridSize: totalSquare 
+  gridLineSize: 2,
+  gridSize: totalSquare
 }
-
-
 
 const KEY = {
   UP: 'ArrowUp',
@@ -28,17 +35,17 @@ const KEY = {
 }
 
 function randomX() {
-  return Math.floor(Math.random() * (canvas.width / sizeSquareMap)) * sizeSquareMap
+  return Math.floor(Math.random() * (canvas.width / sizeSquare)) * sizeSquare
 }
 
 function randomY() {
-  return Math.floor(Math.random() * (canvas.height / sizeSquareMap)) * sizeSquareMap
+  return Math.floor(Math.random() * (canvas.height / sizeSquare)) * sizeSquare
 }
 
 const head = {
   x: randomX(),
   y: randomY(),
-  size: sizeSquareMap,
+  size: sizeSquare,
   dx: 0,
   dy: 0,
   direction: null
@@ -51,25 +58,37 @@ appleImg.src = './apple.png'
 const food = {
   x: randomX(),
   y: randomY(),
-  size: sizeSquareMap,
+  size: sizeSquare,
   appleImg
 }
 
 document.addEventListener('keydown', e => {
   const head = snakeBody[0]
   if (e.key === KEY.UP && head.direction !== KEY.DOWN) {
+    if (head.direction !== e.key) {
+      turnAudio.play()
+    }
     head.dx = 0
     head.dy = -head.size
     head.direction = e.key
   } else if (e.key === KEY.DOWN && head.direction !== KEY.UP) {
+    if (head.direction !== e.key) {
+      turnAudio.play()
+    }
     head.dx = 0
     head.dy = head.size
     head.direction = e.key
   } else if (e.key === KEY.RIGHT && head.direction !== KEY.LEFT) {
+    if (head.direction !== e.key) {
+      turnAudio.play()
+    }
     head.dy = 0
     head.dx = head.size
     head.direction = e.key
   } else if (e.key === KEY.LEFT && head.direction !== KEY.RIGHT) {
+    if (head.direction !== e.key) {
+      turnAudio.play()
+    }
     head.dy = 0
     head.dx = -head.size
     head.direction = e.key
@@ -83,9 +102,10 @@ document.addEventListener('keydown', e => {
 })
 
 function drawSnake() {
-  for (let i = 0; i < snakeBody.length; i++) {
-    ctx.globalCompositeOperation = 'color'
-    ctx.fillStyle = 'green'
+  ctx.fillStyle = '#0E7401'
+  ctx.fillRect(snakeBody[0].x, snakeBody[0].y, snakeBody[0].size, snakeBody[0].size)
+  for (let i = 1; i < snakeBody.length; i++) {
+    ctx.fillStyle = '#56D30A'
     ctx.fillRect(snakeBody[i].x, snakeBody[i].y, snakeBody[i].size, snakeBody[i].size)
   }
 }
@@ -93,32 +113,57 @@ function drawSnake() {
 function drawFood() {
   ctx.globalCompositeOperation = 'color'
   ctx.fillStyle = 'red'
-  ctx.drawImage(food.appleImg,0,0,food.appleImg.width, food.appleImg.height,food.x, food.y, food.size, food.size)
+  ctx.drawImage(
+    food.appleImg,
+    0,
+    0,
+    food.appleImg.width,
+    food.appleImg.height,
+    food.x,
+    food.y,
+    food.size,
+    food.size
+  )
+}
+
+const isFoodOnSnake = foodPosition => {
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeBody[i].x === foodPosition.x && snakeBody[i].y === foodPosition.y) {
+      return true
+    }
+  }
+  return false
 }
 
 function checkColisionFood() {
   const head = snakeBody[0]
   if (head.x === food.x && head.y === food.y) {
+    eatAudio.play()
     snakeBody.push({
-      x: food.x,
-      y: food.y,
-      size: sizeSquareMap,
+      x: snakeBody[snakeBody.length - 1].x,
+      y: snakeBody[snakeBody.length - 1].y,
+      size: sizeSquare,
       dx: 0,
       dy: 0,
       direction: head.direction
     })
     level.score.textContent++
-    // check winner
+
     if (+level.score.textContent >= level.gridSize) {
       level.win = true
       return
     }
-    for (let i = 0; i < snakeBody.length; i++) {
-      while (snakeBody[i].x === food.x && snakeBody[i].y === snakeBody[i].y) {
-        food.x = randomX()
-        food.y = randomY()
+
+    let newFoodPosition
+    do {
+      newFoodPosition = {
+        x: randomX(),
+        y: randomY()
       }
-    }
+    } while (isFoodOnSnake(newFoodPosition))
+
+    food.x = newFoodPosition.x
+    food.y = newFoodPosition.y
   }
 }
 
@@ -153,6 +198,7 @@ function checkColisionSnakeBody() {
   for (let i = 1; i < snakeBody.length - 1; i++) {
     if (snakeBody[i].x === x && snakeBody[i].y === y) {
       level.gameOver = true
+      deadAudio.play()
     }
   }
 }
@@ -179,6 +225,7 @@ function checkGameOver() {
 function checkWin() {
   if (level.win) {
     clearInterval(level.setIntervalId)
+    confetti()
     ctx.globalCompositeOperation = 'source-over'
     ctx.font = '30px Arial'
     ctx.fillStyle = 'white'
@@ -194,12 +241,11 @@ function checkWin() {
 }
 
 function drawGrid() {
-  for (let f = 0; f < canvas.height; f += sizeSquareMap) {
-    for (let c = 0; c < canvas.width; c += sizeSquareMap) {
-      ctx.globalCompositeOperation = 'color'
-      ctx.strokeStyle = '#000'
+  for (let f = 0; f < canvas.height; f += sizeSquare) {
+    for (let c = 0; c < canvas.width; c += sizeSquare) {
+      ctx.strokeStyle = '#fff'
       ctx.lineWidth = level.gridLineSize
-      ctx.strokeRect(f, c, f + sizeSquareMap, c + sizeSquareMap)
+      ctx.strokeRect(f, c, f + sizeSquare, c + sizeSquare)
     }
   }
 }
@@ -209,12 +255,15 @@ function init() {
   drawGrid()
   drawSnake()
   drawFood()
+  moveSnake()
   checkColisionWall()
   checkColisionSnakeBody()
   checkGameOver()
   checkColisionFood()
   checkWin()
-  moveSnake()
 }
 
-level.setIntervalId = setInterval(init, FPS)
+function play() {
+  level.setIntervalId = setInterval(init, interval)
+}
+play()
